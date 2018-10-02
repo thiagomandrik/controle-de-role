44f99fde1b0bc6332595bfe5cfc3d1719227818b
Thiago Mandrik <thiagomandrik@gmail.com>
2018-10-01 22:46:49 -0300

    Implementei o uso de funcoes no codigo

--- scr1.0.py
+++ scr1.0.py
@@ -1,21 +1,27 @@
-print ('')
-print ('#################################################')
-print ('#                                               #')
-print ('# Seja bem vindo ao Sistema de Controle de Rolê #')
-print ('#                                           1.0 #')
-print ('#################################################')
-print ('')
+def bemvindo():
+    print ('')
+    print ('#################################################')
+    print ('#                                               #')
+    print ('# Seja bem vindo ao Sistema de Controle de Rolê #')
+    print ('#                                           1.0 #')
+    print ('#################################################')
+    print ('')
+def prog():
+    participantes = int(input("Quantas pessoas irão participar desse rolê mitológico? "))
+    compraram = int(input("Quantas pessoas compraram as coisas? "))
+    cont = 1
+    valores = []
+    while cont <= compraram:
+        valores.append(float(input("Valor pago pela pessoa %i°: " %cont)))
+        cont+=1
+    valor = 0
 
-participantes = int(input("Quantas pessoas irão participar desse rolê mitológico? "))
-compraram = int(input("Quantas pessoas compraram as coisas? "))
-cont = 1
-valores = []
-while cont <= compraram:
-    valores.append(float(input("Valor pago pela pessoa %i°: " %cont)))
-    cont+=1
-valor = 0
+    for i in valores:
+        valor = float(i) + float(valor)
+    valor = float(valor/participantes)
+    print('Cada um vai pagar R$%.2f pelo rolê mitológico!' %valor)
 
-for i in valores:
-    valor = float(i) + float(valor)
-valor = float(valor/participantes)
-print('Cada um vai pagar R$%.2f pelo rolê mitológico!' %valor)
+
+
+bemvindo()
+prog()
