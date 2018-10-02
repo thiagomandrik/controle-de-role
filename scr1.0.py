dd571f8da267fd35f8f15ac7935c7f29ebdaff95
Thiago Mandrik <thiagomandrik@gmail.com>
2018-10-01 22:03:06 -0300

    Controle de rolê 1.0, teste inicial do GIT

---
+++ scr1.0.py
@@ -0,0 +1,21 @@
+print ('')
+print ('#################################################')
+print ('#                                               #')
+print ('# Seja bem vindo ao Sistema de Controle de Rolê #')
+print ('#                                           1.0 #')
+print ('#################################################')
+print ('')
+
+participantes = int(input("Quantas pessoas irão participar desse rolê mitológico? "))
+compraram = int(input("Quantas pessoas compraram as coisas? "))
+cont = 1
+valores = []
+while cont <= compraram:
+    valores.append(float(input("Valor pago pela pessoa %i°: " %cont)))
+    cont+=1
+valor = 0
+
+for i in valores:
+    valor = float(i) + float(valor)
+valor = float(valor/participantes)
+print('Cada um vai pagar R$%.2f pelo rolê mitológico!' %valor)
